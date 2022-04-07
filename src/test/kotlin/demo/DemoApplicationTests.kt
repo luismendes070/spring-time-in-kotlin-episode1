a2971ae505d070c7818d7225047f5145a1d90300
Luis Mendes <machadoluismendes@gmail.com>
2022-04-07 19:26:57 -0300

    slow shell initialization vscode

--- src/test/kotlin/demo/DemoApplicationTests.kt
+++ src/test/kotlin/demo/DemoApplicationTests.kt
@@ -3,6 +3,8 @@ package demo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+import kotlin.demo.DemoApplication
+
 @SpringBootTest
 class DemoApplicationTests {
 
